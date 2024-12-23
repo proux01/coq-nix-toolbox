e1b9b292e69a75c05eab1200dd454013b3ba071d
Pierre Roux <pierre.roux@onera.fr>
2024-12-23 12:49:45 +0100

    Test stdlib-test

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = "https://github.com/proux01/nixpkgs/archive/b1ad5639c1c55c1004e1b4fadce7837ab68dd3d6.tar.gz";
-         sha256 = "0ly4wn0kyaam59fb3jphkp14sw7bwwar2fsf1r2hash0h16bk4sf";
+         url = "https://github.com/proux01/nixpkgs/archive/db8856ad45897af5ee4360ea643434472b729fdb.tar.gz";
+         sha256 = "0v9kf4wrj9fi2sdsq9r135wr5vqanhqckgdza1mvbyhlcijanviv";
        }
