76e86944978df28dc883acdd871ce807854e65a0
Pierre Roux <pierre.roux@onera.fr>
2025-02-18 17:10:32 +0100

    coqPackages.coqeal: 2.0.3 -> 2.1.0

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/9ea04b91183c9f1854ee19e649f321b83a0ed371.tar.gz";
-    sha256 = "1f7al58wnb74rdhsjqyj4pcldfbzyll13sdl5rqhahgikzifxvhw";
-  }
+         url = "https://github.com/proux01/nixpkgs/archive/8d02b7f5fbf3537e2c46276e03909da41d79ca87.tar.gz";
+         sha256 = "021xnal4q31803pbxqb5g3v7mms2d074d1nkcnslqq8m2r2an4gv";
+       }
